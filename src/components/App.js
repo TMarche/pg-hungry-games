ef2af577b3cb1122045a5ead97d62aeb86b12340
Troy Marchesseault <tmarchess@gmail.com>
2021-08-30 19:42:14 -0400

    Create form with random name generator

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,88 @@
-import React, {useState} from 'react'
+/* eslint-disable jsx-a11y/accessible-emoji */
+import React, {useState} from "react";
+import { Form, Field } from "react-final-form";
+import { uniqueNamesGenerator, Config, names } from "unique-names-generator";
+import faker from "faker";
 
-import Button from "./Button"
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-const App = () => {
-    const [count, setCount] = useState(0);
-    return <div>
-        <Button onClick={() => setCount(count + 1)}>+</Button>
-        <Button onClick={() => setCount(count - 1)}>-</Button>
-        <div>{count}</div>
-    </div>
+const onSubmit = async (values) => {
+  await sleep(300);
+  window.alert(JSON.stringify(values, 0, 2));
+};
+
+const newOnSubmit = (values, current, setter) => {
+    setter([...current, values.name])
 }
 
-export default App;
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            names: [],
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Form
+                    onSubmit={this.onSubmit}
+                    render={({ handleSubmit, form, submitting, pristine, values }) => (
+                    <form onSubmit={ (event) => {
+                        handleSubmit(event);
+                        form.reset();
+                    }}>
+                        <div>
+                        <label>Name</label>
+                        <Field
+                            name="name"
+                            component="input"
+                            type="text"
+                            placeholder="Name"
+                        />
+                        <button
+                            type="button"
+                            onClick={() => this.generateRandomName(form)}
+                        >
+                            Generate Random Name
+                        </button>
+                        </div>
+                        <div>
+                            <button type="submit" disabled={submitting || pristine}>
+                                Submit
+                            </button>
+                            <button
+                                type="button"
+                                onClick={form.reset}
+                                disabled={submitting || pristine}
+                            >
+                                Reset
+                            </button>
+                        </div>
+                        <pre>{JSON.stringify(values, 0, 2)}</pre>
+                    </form>
+                    )}
+                />
+                {this.state.names.map( x => <div>{x}</div>)}
+            </div>
+        )
+    }
+
+    generateRandomName = (form) => {
+       form.change("name", faker.name.findName());
+    }
+
+    onSubmit = (values) => {
+        this.addName(values.name)
+    }
+
+    addName = (name) => {
+        this.setState({
+            names: [...this.state.names, name]
+        })
+    }
+}
+
+export default App
